9b3165a70c92c942b9adbd41a1d143f7bc20c1fe
Scott Balun <sbalun@gmail.com>
2021-02-26 01:18:42 -0500

    chapter3d exercises. if/else/if statements

---
+++ src/chapter3/TestResults.java
@@ -0,0 +1,40 @@
+package chapter3;
+/*
+IF - ELSE - IF Statements
+Who? A Student
+What? wants to know their corresponding letter grade given a number grade
+Why? so they may know the letter grade they will get on their report card
+ */
+import java.util.Scanner;
+
+public class TestResults {
+
+    public static void main(String[] args){
+
+        // Get the test score
+        System.out.println("Yo, what's your test score?");
+        Scanner scanner = new Scanner(System.in);
+        double testScore = scanner.nextDouble();
+        scanner.close();
+
+        // Determine the letter grade
+        char grade;
+
+        if (testScore < 60) {
+            grade = 'F';
+        }
+        else if (testScore < 70) {
+            grade = 'D';
+        }
+        else if (testScore < 80) {
+            grade = 'C';
+        }
+        else if (testScore < 90) {
+            grade = 'B';
+        }
+        else{
+            grade = 'A';
+        }
+        System.out.println("Your score of " + testScore + " resulted in a grade of " + grade);
+    }
+}
