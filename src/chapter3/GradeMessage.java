8b01caa1979e7b9197370d49c8387fb54f171c0e
Scott Balun <sbalun@gmail.com>
2021-03-02 09:12:54 -0500

    chapter3e exercises. switch statements

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -3,10 +3,13 @@ package chapter3;
 import java.util.Scanner;
 
 /*
+Chapter 3e
 Using the SWITCH statement.
 - Similar to if - else - if.
 - The difference between the if-else-if and the switch statements is that the if-else-if
      checks the condition to be true; whereas the switch statement checks for equality.
+     Most of the time it's better to implement if else if versus switch because if else allows
+     for a range of conditions not just equality
  */
 public class GradeMessage {
 
@@ -35,10 +38,12 @@ public class GradeMessage {
                 break;
             case "F":
                 message = "Oh boy, try studying even more";
+                break;
             default:
                 message = "Error! Invalid grade";
-
-        System.out.println("You had a grade of " + grade + "  " + message);
+                break;
         }
+        System.out.println("You had a grade of " + grade + "  " + message);
+
     }
 }
