cd019ec5945aac8e0763759ca32d2f8ced36fc5a
Scott Balun <sbalun@gmail.com>
2021-03-10 23:59:05 -0500

    Chapter 5 - Optional Exercise - Phone Bill Calculator

---
+++ src/chapter5/PhoneBillCalculator.java
@@ -0,0 +1,22 @@
+package chapter5;
+
+// Chapter 5 - Optional Exercise
+// Calculate the final total of a cell phone bill.
+// Allow the user to put in the plan fee and the number of overage minutes
+// Charge the user 0.25 for every minute they were over their plan
+// Charge 15% tax on the subtotal
+// Create separate methods to calculate the tax, overage fees, and final total.
+// Print the itemized bill
+
+public class PhoneBillCalculator {
+
+    // Main method
+
+    // calculateTax method
+
+    // calculateOverageFees method
+
+    // getBillTotal method
+
+    // printBill method
+}
