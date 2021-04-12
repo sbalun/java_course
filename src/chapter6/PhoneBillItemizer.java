7aff2ee4c3743f00b009f96caaed1214155fd00b
Scott Balun <sbalun@gmail.com>
2021-04-12 09:42:49 -0400

    Chapter 7 Array Exercises

--- src/chapter6/PhoneBillItemizer.java
+++ src/chapter6/PhoneBillItemizer.java
@@ -3,7 +3,7 @@ package chapter6;
 public class PhoneBillItemizer {
 
     public static void main(String[] args) {
-        PhoneBill phoneBill1 = new PhoneBill(882122, 125, 100, 95);
+        PhoneBill phoneBill1 = new PhoneBill(882122, 125, 100, 1357);
         System.out.println(phoneBill1.printBill());
     }
 }
---
+++ src/chapter7/LotteryTicket.java
@@ -0,0 +1,4 @@
+package chapter7;
+
+public class LotteryTicket {
+}
