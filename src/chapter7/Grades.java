bafb7ad9b2286dc6522058d325c2fc5af2de7ad5
Scott Balun <sbalun@gmail.com>
2021-06-08 18:28:58 -0400

    Chapter 7c WIP Useful Tricks for Arrays - More Work

--- src/chapter7/Grades.java
+++ src/chapter7/Grades.java
@@ -8,20 +8,59 @@ import java.util.Scanner;
  */
 public class Grades {
 
+    // We need an array in which to store our grades
     private static int grades[];
+    // We need a scanner to get user input
     private static Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args){
-        System.out.println("How many grades you wanna enter, homey?");
+        System.out.println("How many grades you wanna enter, homey? ");
         grades = new int[myScanner.nextInt()];
-
         getGrades();
-
-        public static void getGrades(){
-            // TBD
-        }
-
-
+        System.out.println("Average: " + String.format("%.2f", calculateAverage()));
+        System.out.println("Highest: " + String.format("%.2f", getHighest()));
+        System.out.println("Lowest: " + String.format("%.2f", getlowest()));
 
     }
+    public static void getGrades(){
+        for(int i = 0; i < grades.length; i++){
+            System.out.println("Please enter grade #"+ (i+1) + ":");
+            // We declared the array grades as a class variable so we can use it here
+            grades[i] = myScanner.nextInt();
+        }
+    }
+
+    public static int calculateSum(){
+        int sum = 0;
+        for(int grade : grades){
+            sum = sum + grade;
+        }
+        return sum;
+    }
+
+    public static double calculateAverage(){
+        return calculateSum()/grades.length;
+    }
+
+    //5:57 in lesson 7c
+    public static double getHighest(){
+        int highest = grades[0];
+        for(int grade : grades){
+            if(grade > highest){
+                highest = grade;
+            }
+        }
+        return highest;
+    }
+
+    public static double getlowest(){
+        int lowest = grades[0];
+        for(int grade : grades){
+            if(grade < lowest){
+                lowest = grade;
+            }
+        }
+        return lowest;
+    }
+
 }
