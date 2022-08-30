27f89c4f6f6434e6eb19a2009287f08c55f5e600
Scott Balun <sbalun@gmail.com>
2022-08-30 17:57:28 -0400

    chapter 9 project - add employee class which inherits person

---
+++ src/chapter9/Employee.java
@@ -0,0 +1,23 @@
+package chapter9;
+
+public class Employee extends Person {
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String employeeId;
+    private String title;
+}
