cfbe1066b3d4788539a12728434cc47f4ae1d9a3
Scott Balun <sbalun@gmail.com>
2022-09-16 18:43:15 -0400

    ch9c adding rectangle class incl getters and setters

---
+++ src/chapter9/Rectangle.java
@@ -0,0 +1,36 @@
+package chapter9;
+
+public class Rectangle {
+
+    protected double length;
+    protected double width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getSides() {
+        return sides;
+    }
+
+    public void setSides(double sides) {
+        this.sides = sides;
+    }
+
+    protected double sides = 4;
+
+
+
+}
