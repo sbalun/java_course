d3f785b8c653b8b6ec3082222972000a23fd6222
Scott Balun <sbalun@gmail.com>
2022-12-03 17:27:17 -0500

    chapter 10 polymorphism

--- src/chapter10/Zoo.java
+++ src/chapter10/Zoo.java
@@ -2,14 +2,17 @@ package chapter10;
 
 public class Zoo {
     public static void main(String[] args) {
+
         Dog pepper = new Dog();
         pepper.makeSound();
         pepper.fetch();
+        feed(pepper);
 
         Cat garfield = new Cat();
         garfield.makeSound();
         garfield.scratch();
 
+        // Superclass is Animal. Instantiate as Dog
         Animal lance = new Dog();
         lance.makeSound();
 
@@ -17,6 +20,18 @@ public class Zoo {
         snoopy.makeSound();
         snoopy = new Cat();
         snoopy.makeSound();
-        ((Cat) snoopy).scratch();
+        ((Cat)snoopy).scratch();
+        feed(snoopy);
+    }
+
+    public static void feed (Animal animal){
+        // Find out what the subclass is by doing a check with instaneof
+        // whatever is on the left, i.e., animal, instanceof will check to see if it is of subclass, Dog
+        // returns boolean value
+        if(animal instanceof Dog){
+            System.out.println("here is your dog food");
+        }else if(animal instanceof Cat){
+            System.out.println("here is your cat food");
+        }
     }
 }
