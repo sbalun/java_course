c01c111fcbc155ecbfd8855b5c960d30fe037591
Scott Balun <sbalun@gmail.com>
2022-12-04 07:36:39 -0500

    chapter 11 abstraction

--- src/chapter11/Shape.java
+++ src/chapter11/Shape.java
@@ -4,11 +4,20 @@ package chapter11;
 *--------------*
 |  Abstraction |
 * -------------*
- - The reserved word, abstract, is a non-access modifier that can be used on classes and methods.
- - Used when you want to define a template for a class or a method, but do not intend for it to be used as is.
- - Not designed to be instantiated.  Meant to be extended.  Subclass must implement.
+     - This reserved word is a non-access modifier that can be used on classes and methods.
+     - Used when you want to define a template for a class or a method, but do not intend for it to be used as is.
+     - Not designed to be instantiated.  Meant to be extended.  Subclass must implement.
+     - Serves as a template
+*------------------*
+|  Abstract Method |
+* -----------------*
+     - Has no body.  Only signature is defined.
+     - Not to be run as is
+     - Meant to be overridden by a sub class
  */
 
 public abstract class Shape {
+
+    abstract double calculateArea();
 
 }
