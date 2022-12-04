05bbb8274260249f5f8f6185c1edcd49a45919a3
Scott Balun <sbalun@gmail.com>
2022-12-04 07:53:49 -0500

    chapter 11 abstraction

--- src/chapter11/Shape.java
+++ src/chapter11/Shape.java
@@ -7,14 +7,14 @@ package chapter11;
      - This reserved word is a non-access modifier that can be used on classes and methods.
      - Used when you want to define a template for a class or a method, but do not intend for it to be used as is.
      - Not designed to be instantiated.  Meant to be extended.  Subclass must implement.
-     - Serves as a template
+     - Serves as a template not implementation
      - Can contain both abstract and implemented methods
 *------------------*
 |  Abstract Method |
 * -----------------*
      - Has no body.  Only signature is defined.
      - Not to be run as is
-     - Meant to be overridden by a sub class
+     - Meant to be overridden by a sub-class
  */
 
 public abstract class Shape {
---
+++ src/chapter11/ShapeTester.java
@@ -0,0 +1,10 @@
+package chapter11;
+
+public class ShapeTester {
+
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(12,11);
+        rectangle.print();
+        System.out.println("The area of the rectangle is " + rectangle.calculateArea());
+    }
+}
