c34ab1c12258e5741dbccefba9fe95e7e3418704
Scott Balun <sbalun@gmail.com>
2022-12-07 11:52:51 -0500

    chapter 12a collections exercises

--- src/chapter12a/Maps.java
+++ src/chapter12a/Maps.java
@@ -1,4 +1,28 @@
 package chapter12a;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Maps {
+
+    public static void main(String[] args) {
+        // Maps use Key - Value pairs
+        // You access an element using it's key.  Keys are unique.  Values aren't
+        // The put method needs a "key" and a "value"
+
+        Map fruitCalories = new HashMap();
+        fruitCalories.put("apple",95);
+        fruitCalories.put("banana",120);
+        fruitCalories.put("cherry",85);
+        fruitCalories.put("lemon",200);
+        fruitCalories.put("mango",75);
+
+        System.out.println(fruitCalories); // {banana=120, apple=95, cherry=85, lemon=200, mango=75}
+        System.out.println(fruitCalories.size()); // 5
+        System.out.println(fruitCalories.get("cherry")); // 85
+        System.out.println(fruitCalories.entrySet()); // [banana=120, apple=95, cherry=85, lemon=200, mango=75]
+        System.out.println(fruitCalories.keySet()); // [banana, apple, cherry, lemon, mango]
+        System.out.println(fruitCalories.containsKey("kiwi")); // false
+        System.out.println(fruitCalories.containsKey("mango")); // true
+    }
 }
