b3aacfe0a71f72064e58e45746bdddfbbf51dc1c
Scott Balun <sbalun@gmail.com>
2022-12-14 18:27:16 -0500

    Chapter 13a  Exception Handling

--- src/chapter13a/ExceptionHandling.java
+++ src/chapter13a/ExceptionHandling.java
@@ -1,7 +1,9 @@
 package chapter13a;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionHandling {
@@ -12,26 +14,37 @@ public class ExceptionHandling {
         numbersExceptionHandling();
     }
 
-    private static void numbersExceptionHandling() {
-
-        File file = new File("resources/nonexistent.txt");
+    public static void numbersExceptionHandling(){
+        File file = new File("resources/numbers.txt");
+        Scanner fileReader = null;
         try{
-            Scanner fileReader = new Scanner(file);
+            fileReader = new Scanner(file);
 
             while(fileReader.hasNext()){
-                double num = 
+                double num = fileReader.nextDouble();
+                System.out.println(num);
             }
-        }catch(fileNotFoundException e) {
-            System.out.println();
+        }catch(FileNotFoundException | InputMismatchException e){
+            e.printStackTrace();
+            //Even if you finish everything in the try,
+            //even if no exceptions are thrown, anything
+            //placed inside finally will execute no matter what
+        }finally{
+            fileReader.close();
         }
     }
 
     private static void createNewFile() {
 
         File file = new File("resources/nonexistent.txt");
-        try{
+        try {
             file.createNewFile();
-        }catch (Exception e){
+            // - Use the Exception object in the catch block as it is the super class
+            // of all the other exceptions which inherit from Exception so you can
+            // use the superclass object, Exeption, as a catch-all
+            //
+            // - When an exception is caught it provides a substack trace as well
+        } catch (Exception e) {
             System.out.println("Directory does not exist homey");
             e.printStackTrace();
         }
