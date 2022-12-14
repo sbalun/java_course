18700ffae2a7fb822b03cd344fec7d77ebeb14b3
Scott Balun <sbalun@gmail.com>
2022-12-13 21:35:10 -0500

    Revert "Chapter 12a  Exception Handling"
    This reverts commit 3412ce3a78dc550d8eed2b4e15579ff80177d6e5.

--- src/chapter13a/ExceptionHandling.java
+++ src/chapter13a/ExceptionHandling.java
@@ -1,41 +1,9 @@
 package chapter13a;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class ExceptionHandling {
 
     public static void main(String[] args) {
 
-        //createNewFile();
-        numbersExceptionHandling();
-    }
-
-    private static void numbersExceptionHandling() {
-
-        File file = new File("resources/nonexistent.txt");
-        try{
-            Scanner fileReader = new Scanner(file);
-
-            while(fileReader.hasNext()){
-                double num = fileReader.nextDouble();
-                System.out.println(num);
-            }
-        }catch(fileNotFoundException e) {
-            System.out.println();
-        }
-    }
-
-    private static void createNewFile() {
-
-        File file = new File("resources/nonexistent.txt");
-        try{
-            file.createNewFile();
-        }catch (Exception e){
-            System.out.println("Directory does not exist homey");
-            e.printStackTrace();
-        }
     }
 
 }
